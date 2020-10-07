89642a1fdb937ade5639e7c9f03a96def925cd6a
Aline Soares da Silva <aline.as385@gmail.com>
2020-10-06 22:09:06 -0300

    select

---
+++ sql-selector.sql
@@ -0,0 +1,17 @@
+-- Escreva abaixo o comando SQL que busca todas as pessoas.
+SELECT * FROM Pessoas;
+
+-- Escreva abaixo o comando SQL que busca apenas as cidades.
+SELECT cidade FROM Pessoas;
+
+-- Escreva abaixo o comando SQL que busca apenas as editoras e apresenta uma vez.
+SELECT DISTINCT editora FROM Livros;
+
+-- Escreva abaixo o comando SQL que busca apenas os nomes das pessoas.
+SELECT nome FROM Pessoas;
+
+-- Escreva abaixo o comando SQL que busca apenas os títulos dos livros.
+SELECT titulo FROM Livros;
+
+-- Escreva abaixo o comando SQL que resgata cada cidade apenas uma vez.
+SELECT DISTINCT cidade FROM Pessoas;
